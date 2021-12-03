eadd9f6fd9484d18051483260ca412ca347a35f5
Heidi Hlavinka <hchlav@gmail.com>
2021-12-02 21:09:34 -0600

    commit tree bb13b6a6e7a67ef54064f065859fedb42f87ab7a

--- lib/operations.rb
+++ lib/operations.rb
@@ -4,14 +4,14 @@ def unsafe?(speed)
     else speed >= 40 && speed <= 60
         false
     end
-
 end
 
 
 
 def not_safe?(speed)
-	
+	speed > 60 || speed < 40 ? true : false
+
 end
 	
-
+# conditional ? action_if_true : action_if_false
 
